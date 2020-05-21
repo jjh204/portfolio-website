08c2d3c5e08e6e9962c2057d584be0f8a5d5d11e
jjh204 <59932565+jjh204@users.noreply.github.com>
2020-05-21 15:22:32 +0200

    updated javascript file after prettier

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,21 @@
-(function() {
-  var form = document.querySelector('#contact-form');
-  var formConfirm = document.querySelector('#form-sent');
-  var nameInput = document.querySelector('#contact-name');
-  var emailInput = document.querySelector('#contact-email');
-  var messageInput = document.querySelector('#contact-message');
+(function () {
+  var form = document.querySelector("#contact-form");
+  var formConfirm = document.querySelector("#form-sent");
+  var nameInput = document.querySelector("#contact-name");
+  var emailInput = document.querySelector("#contact-email");
+  var messageInput = document.querySelector("#contact-message");
 
   function showErrorMessage(input, message) {
     var container = input.parentElement;
 
-    var error = container.querySelector('.error-message');
+    var error = container.querySelector(".error-message");
     if (error) {
       container.removeChild(error);
     }
 
     if (message) {
-      var error = document.createElement('div');
-      error.classList.add('error-message');
+      var error = document.createElement("div");
+      error.classList.add("error-message");
       error.innerText = message;
       container.appendChild(error);
     }
@@ -25,12 +25,12 @@
     var value = nameInput.value;
 
     if (!value) {
-      showErrorMessage(nameInput, 'A name is required.');
+      showErrorMessage(nameInput, "A name is required.");
       return false;
     }
 
     if (value.length < 1) {
-      showErrorMessage(nameInput, 'A name is required.');
+      showErrorMessage(nameInput, "A name is required.");
       return false;
     }
 
@@ -42,12 +42,12 @@
     var value = emailInput.value;
 
     if (!value) {
-      showErrorMessage(emailInput, 'An e-mail address is required.');
+      showErrorMessage(emailInput, "An e-mail address is required.");
       return false;
     }
 
-    if (value.indexOf('@') === -1) {
-      showErrorMessage(emailInput, 'You must enter a valid e-mail address.');
+    if (value.indexOf("@") === -1) {
+      showErrorMessage(emailInput, "You must enter a valid e-mail address.");
       return false;
     }
 
@@ -59,12 +59,12 @@
     var value = messageInput.value;
 
     if (!value) {
-      showErrorMessage(messageInput, 'A message is required.');
+      showErrorMessage(messageInput, "A message is required.");
       return false;
     }
 
     if (value.length < 1) {
-      showErrorMessage(messageInput, 'A message is required.');
+      showErrorMessage(messageInput, "A message is required.");
       return false;
     }
 
@@ -73,22 +73,22 @@
   }
 
   function validateForm() {
-    var isValidName = validateName ();
-    var isValidEmail = validateEmail ();
-    var isValidMessage = validateMessage ();
+    var isValidName = validateName();
+    var isValidEmail = validateEmail();
+    var isValidMessage = validateMessage();
     return isValidName && isValidEmail && isValidMessage;
   }
 
-  form.addEventListener('submit', (e) => {
+  form.addEventListener("submit", (e) => {
     e.preventDefault();
     if (validateForm()) {
-      alert('Your message has been sent.');
-      form.classList.add('is-hidden');
-      formConfirm.classList.remove('is-hidden');
+      alert("Your message has been sent.");
+      form.classList.add("is-hidden");
+      formConfirm.classList.remove("is-hidden");
     }
   });
 
-  nameInput.addEventListener('input', validateName);
-  emailInput.addEventListener('input', validateEmail);
-  messageInput.addEventListener('input', validateMessage);
+  nameInput.addEventListener("input", validateName);
+  emailInput.addEventListener("input", validateEmail);
+  messageInput.addEventListener("input", validateMessage);
 })();
